a5d5cc9a8d39e9aa2bf9516da83138b6dd965544
Talha <brandsmakr8@gmail.com>
2022-06-18 08:47:09 -0700

    corona casses on map

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
-function updateMap()
-{
-    
+function updateMap() {
+  fetch("/data.json")
+    .then((res) => res.json())
+    .then((info) => {
+      console.log(info);
+      info.data.forEach((element) => {
+        latitude = element.latitude;
+        longitude = element.longitude;
+
+        cases = element.infected;
+        if(cases>255)
+        {
+            var marker = new mapboxgl.Marker({
+                draggable: false,
+                color : "rgb(255, 0, 0)"
+              })
+                .setLngLat([longitude, latitude])
+                .addTo(map);
+            
+        }
+        else
+        {
+            var marker = new mapboxgl.Marker({
+                draggable: false,
+                color: `rgb(${cases}, 0, 0)`
+              })
+                .setLngLat([longitude, latitude])
+                .addTo(map);
+        }
+
+        // Mark on the map
+        // var marker = new mapboxgl.Marker({
+        //   draggable: false,
+        // //   color: "rgb(132, 121, 129)"
+        // })
+        //   .setLngLat([longitude, latitude])
+        //   .addTo(map);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 updateMap();
